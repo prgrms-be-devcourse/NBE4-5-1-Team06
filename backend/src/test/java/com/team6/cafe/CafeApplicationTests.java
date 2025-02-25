cd215babfef5b69df17ab40848f58dabbac88263
cloudmato <cloudmato00@gmail.com>
2025-02-25 15:21:55 +0900

    [Test] 각 테스트 메소드 실행시마다 데이터가 쌓이는 문제 해결

--- backend/src/test/java/com/team6/cafe/CafeApplicationTests.java
+++ backend/src/test/java/com/team6/cafe/CafeApplicationTests.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -31,6 +32,7 @@ import com.team6.cafe.domain.orderCoffee.dto.OrderCoffeeResponseDto;
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CafeApplicationTests {
 	@Autowired
 	private OrderService orderService;
